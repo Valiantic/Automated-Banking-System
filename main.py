df7df6992461b53cfc403157fe56a72a099844ae
StevenMadali <stevenmadali17@gmail.com>
2024-07-23 20:59:13 +0800

    finish prototyping

--- main.py
+++ main.py
@@ -26,3 +26,59 @@ def main():
         print("6. Exit")
         
         choice = input("Enter your choice:")
+        
+        if choice == "1":
+            account_number = len(accounts) + 1
+            initial_balance = float(input("Enter initial balance: "))
+            accounts[account_number] = Account(account_number, initial_balance)
+            print("Account created with number:", account_number)
+            
+        elif choice == "2":
+            account_number = int(input("Enter account number: "))
+            account = accounts.get(account_number)
+            if account:
+                print("Balance", account.balance)
+            else:
+                print("Account not found.") 
+                
+        elif choice == "3":
+            account_number = int(input("Enter account number: "))
+            account = accounts.get(account_number)
+            if account:
+                amount = float(input("Enter deposit amount: "))
+                account.deposit(amount)
+                print("Deposited: ", amount)
+            else:
+                print("Account not found")
+                
+        elif choice == "4":
+            account_number = int(input("Enter account number: "))
+            account = accounts.get(account_number)
+            if account:
+                amount = float(input("Enter withdrawal amount: "))
+                account.withdraw(amount)
+                print("Withdrawn: ", amount)
+            else:
+                print("Account not found")
+            
+                 
+        elif choice == "5":
+            account_number = int(input("Enter account number: "))
+            account = accounts.get(account_number)
+            if account:
+                print("Account Number: ", account.account_number)
+                print("Balance: ", account.balance)
+            else:
+                print("Account not found")
+                
+        elif choice == "6":
+            print("Exciting...")
+            break
+        
+        else:
+                print("Invalid choice. Please try again.")
+                
+            
+if __name__ == '__main__':   
+    main()
+            
